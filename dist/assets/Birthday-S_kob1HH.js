c856395219893a3ffe0517a10e5d1d4adf291c0a
Rannie Raymundo <rraymundo@krmsupport.com>
2025-02-03 01:19:14 +0800

    build the project

---
+++ dist/assets/Birthday-S_kob1HH.js
@@ -0,0 +1,1 @@
+import{_ as t,c as r,o as a,r as o}from"./index-DfuKFlRJ.js";const n={name:"birthday"};function s(e,c,l,d,p,_){return a(),r("main",null,[o(e.$slots,"default")])}const i=t(n,[["render",s]]);export{i as default};
